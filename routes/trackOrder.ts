bd49d633da7d34ca083633f8001725b90fc64a61
Matthis <matthisphan.pro@gmail.com>
2025-12-03 18:21:05 +0100

    fix Code injection

--- routes/trackOrder.ts
+++ routes/trackOrder.ts
@@ -2,7 +2,6 @@
  * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
  * SPDX-License-Identifier: MIT
  */
-
 import * as utils from '../lib/utils'
 import * as challengeUtils from '../lib/challengeUtils'
 import { type Request, type Response } from 'express'
@@ -11,19 +10,33 @@ import { challenges } from '../data/datacache'
 
 export function trackOrder () {
   return (req: Request, res: Response) => {
-    // Truncate id to avoid unintentional RCE
-    const id = !utils.isChallengeEnabled(challenges.reflectedXssChallenge) ? String(req.params.id).replace(/[^\w-]+/g, '') : utils.trunc(req.params.id, 60)
+    // Sanitize or truncate ID depending on challenge mode
+    const id = !utils.isChallengeEnabled(challenges.reflectedXssChallenge)
+      ? String(req.params.id).replace(/[^\w-]+/g, '')
+      : utils.trunc(req.params.id, 60)
 
-    challengeUtils.solveIf(challenges.reflectedXssChallenge, () => { return utils.contains(id, '<iframe src="javascript:alert(`xss`)">') })
-    db.ordersCollection.find({ $where: `this.orderId === '${id}'` }).then((order: any) => {
-      const result = utils.queryResultToJson(order)
-      challengeUtils.solveIf(challenges.noSqlOrdersChallenge, () => { return result.data.length > 1 })
-      if (result.data[0] === undefined) {
-        result.data[0] = { orderId: id }
-      }
-      res.json(result)
-    }, () => {
-      res.status(400).json({ error: 'Wrong Param' })
-    })
+    challengeUtils.solveIf(
+      challenges.reflectedXssChallenge,
+      () => utils.contains(id, '<iframe src="javascript:alert(`xss`)">')
+    )
+
+    db.ordersCollection
+      .find({ orderId: id })
+      .then((order: any) => {
+        const result = utils.queryResultToJson(order)
+        challengeUtils.solveIf(
+          challenges.noSqlOrdersChallenge,
+          () => result.data.length > 1
+        )
+
+        if (result.data[0] === undefined) {
+          result.data[0] = { orderId: id }
+        }
+
+        res.json(result)
+      })
+      .catch(() => {
+        res.status(400).json({ error: 'Wrong Param' })
+      })
   }
 }
